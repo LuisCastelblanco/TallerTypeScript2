66f5783565cb2728ac7793c93cfb499138275820
Luis Castelblanco <la.castelblanco@uniandes.edu.co>
2023-10-21 20:37:09 -0500

    New Features and Last Design improvement

--- main.ts
+++ main.ts
@@ -1,42 +1,65 @@
-import { serie } from './serie.js';
-import { Data } from './data.js';
+import {serie} from './serie.js';
+import {Data} from './data.js';
 
 const seriesTbody = document.getElementById('series') as HTMLElement;
-const averageElement = document.getElementById("average") as HTMLElement;
+const containerCard = document.getElementById("container-card") as HTMLElement;
+let averageSeasons = 0;
 
-loadSeries(Data);
+initialize();
 
-function loadSeries(seriesData: serie[]): void {
-    let totalSeasons: number = 0;
+function initialize() {
+    populateSeriesTable(Data);
+    bindTableClickListener();
+}
 
-    console.log("Loading series...");
-
-    seriesData.forEach((seriesItem) => {
-        totalSeasons += appendSeriesToTable(seriesItem);
-        console.log(`Series ${seriesItem.num} loaded`);
+function populateSeriesTable(series: serie[]): void {
+    console.log("Populating series...");
+    series.forEach(serie => {
+        const row = generateSerieRow(serie);
+        seriesTbody.appendChild(row);
+        averageSeasons += serie.seasons;
+        console.log(`Processed Serie number: ${serie.num}`);
     });
-
-    const avgSeasons = calculateAverage(totalSeasons, seriesData.length);
-    averageElement.innerHTML = `Seasons Average: ${avgSeasons}`;
-
-    console.log("Loading complete");
+    displayAverageSeasons(series);
+    console.log("Series population complete.");
 }
 
-function appendSeriesToTable(seriesItem: serie): number {
-    const trElement = document.createElement('tr');
-
-    trElement.innerHTML = `
-        <th>${seriesItem.num}</th>
-        <td class="text-primary">${seriesItem.name}</td>
-        <td>${seriesItem.channel}</td>
-        <td>${seriesItem.seasons}</td>
-    `;
-
-    seriesTbody.appendChild(trElement);
-
-    return seriesItem.seasons;
+function bindTableClickListener(): void {
+    seriesTbody.addEventListener("click", handleRowClick);
 }
 
-function calculateAverage(total: number, count: number): number {
-    return total / count;
+function handleRowClick(event: Event): void {
+    const clickedElement = event.target as HTMLElement;
+    if (['TH', 'TD'].includes(clickedElement.tagName)) {
+        const row = clickedElement.closest('tr') as HTMLTableRowElement;
+        const id = parseInt(row.cells[0].textContent!);
+        const selectedSerie = Data.find(serie => serie.num === id);
+        if (selectedSerie) displaySerieDetails(selectedSerie);
+    }
+}
+
+function generateSerieRow(serie: serie): HTMLTableRowElement {
+    const row = document.createElement('tr');
+    row.innerHTML = `<th>${serie.num}</th>
+                     <td class="text-primary">${serie.name}</td>
+                     <td>${serie.channel}</td>
+                     <td>${serie.seasons}</td>`;
+    return row;
+}
+
+function displaySerieDetails(serie: serie): void {
+    containerCard.innerHTML = `
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="${serie.img}" alt="Serie image">
+            <div class="card-body">
+                <h5 class="card-title" style="font-weight: bold;">${serie.name}</h5>
+                <p class="card-text">${serie.description}</p>
+                <a href="${serie.url}" target="_blank">${serie.url}</a>
+            </div>
+        </div>`;
+}
+
+function displayAverageSeasons(series: serie[]): void {
+    averageSeasons /= series.length;
+    document.getElementById("average")!.textContent = `Seasons Average: ${averageSeasons}`;
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,45 @@
 import { Data } from './data.js';
 var seriesTbody = document.getElementById('series');
-var averageElement = document.getElementById("average");
-loadSeries(Data);
-function loadSeries(seriesData) {
-    var totalSeasons = 0;
-    console.log("Loading series...");
-    seriesData.forEach(function (seriesItem) {
-        totalSeasons += appendSeriesToTable(seriesItem);
-        console.log("Series ".concat(seriesItem.num, " loaded"));
+var containerCard = document.getElementById("container-card");
+var averageSeasons = 0;
+initialize();
+function initialize() {
+    populateSeriesTable(Data);
+    bindTableClickListener();
+}
+function populateSeriesTable(series) {
+    console.log("Populating series...");
+    series.forEach(function (serie) {
+        var row = generateSerieRow(serie);
+        seriesTbody.appendChild(row);
+        averageSeasons += serie.seasons;
+        console.log("Processed Serie number: ".concat(serie.num));
     });
-    var avgSeasons = calculateAverage(totalSeasons, seriesData.length);
-    averageElement.innerHTML = "Seasons Average: ".concat(avgSeasons);
-    console.log("Loading complete");
+    displayAverageSeasons(series);
+    console.log("Series population complete.");
 }
-function appendSeriesToTable(seriesItem) {
-    var trElement = document.createElement('tr');
-    trElement.innerHTML = "\n        <th>".concat(seriesItem.num, "</th>\n        <td class=\"text-primary\">").concat(seriesItem.name, "</td>\n        <td>").concat(seriesItem.channel, "</td>\n        <td>").concat(seriesItem.seasons, "</td>\n    ");
-    seriesTbody.appendChild(trElement);
-    return seriesItem.seasons;
+function bindTableClickListener() {
+    seriesTbody.addEventListener("click", handleRowClick);
 }
-function calculateAverage(total, count) {
-    return total / count;
+function handleRowClick(event) {
+    var clickedElement = event.target;
+    if (['TH', 'TD'].includes(clickedElement.tagName)) {
+        var row = clickedElement.closest('tr');
+        var id_1 = parseInt(row.cells[0].textContent);
+        var selectedSerie = Data.find(function (serie) { return serie.num === id_1; });
+        if (selectedSerie)
+            displaySerieDetails(selectedSerie);
+    }
+}
+function generateSerieRow(serie) {
+    var row = document.createElement('tr');
+    row.innerHTML = "<th>".concat(serie.num, "</th>\n                     <td class=\"text-primary\">").concat(serie.name, "</td>\n                     <td>").concat(serie.channel, "</td>\n                     <td>").concat(serie.seasons, "</td>");
+    return row;
+}
+function displaySerieDetails(serie) {
+    containerCard.innerHTML = "\n        <div class=\"card\" style=\"width: 18rem;\">\n            <img class=\"card-img-top\" src=\"".concat(serie.img, "\" alt=\"Serie image\">\n            <div class=\"card-body\">\n                <h5 class=\"card-title\" style=\"font-weight: bold;\">").concat(serie.name, "</h5>\n                <p class=\"card-text\">").concat(serie.description, "</p>\n                <a href=\"").concat(serie.url, "\" target=\"_blank\">").concat(serie.url, "</a>\n            </div>\n        </div>");
+}
+function displayAverageSeasons(series) {
+    averageSeasons /= series.length;
+    document.getElementById("average").textContent = "Seasons Average: ".concat(averageSeasons);
 }
